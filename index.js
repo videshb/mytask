3ba21964098d5a3998cc3ab9b08c0028322ebbaf
EC2 Default User <ec2-user@ip-172-31-77-228.ec2.internal>
2025-11-15 10:35:03 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Hello from myapp!'));
+app.listen(port, () => console.log(`Listening on ${port}`));
+
